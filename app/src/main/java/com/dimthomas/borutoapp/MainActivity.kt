514fba3744fdb3e2fcb2af791f39f933b2369ca0
Dmitriy Averyanov <dimthomas1989@gmail.com>
2024-06-15 19:47:20 +0300

    Added Navigation

--- app/src/main/java/com/dimthomas/borutoapp/MainActivity.kt
+++ app/src/main/java/com/dimthomas/borutoapp/MainActivity.kt
@@ -10,14 +10,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.dimthomas.borutoapp.navigation.SetupNavGraph
 import com.dimthomas.borutoapp.ui.theme.BorutoAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BorutoAppTheme {
-
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
