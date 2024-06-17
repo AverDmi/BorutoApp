06a15d6504d62327498723e744735d34241ac4ac
Dmitriy Averyanov <dimthomas1989@gmail.com>
2024-06-17 20:21:26 +0300

    Added DatabaseConverter

--- app/src/main/java/com/dimthomas/borutoapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/dimthomas/borutoapp/data/local/BorutoDatabase.kt
@@ -2,12 +2,15 @@ package com.dimthomas.borutoapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.dimthomas.borutoapp.data.local.dao.HeroDao
 import com.dimthomas.borutoapp.data.local.dao.HeroRemoteKeyDao
 import com.dimthomas.borutoapp.domain.model.Hero
 import com.dimthomas.borutoapp.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+
+@TypeConverters(DataBaseConverter::class)
 abstract class BorutoDatabase: RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
