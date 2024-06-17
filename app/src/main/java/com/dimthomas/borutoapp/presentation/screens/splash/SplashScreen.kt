ec94453e83c47e755f09c348a48bfc4f10128f82
Dmitriy Averyanov <dimthomas1989@gmail.com>
2024-06-17 22:00:38 +0300

    Added Animate Splash Screen

--- app/src/main/java/com/dimthomas/borutoapp/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/dimthomas/borutoapp/presentation/screens/splash/SplashScreen.kt
@@ -1,52 +1,73 @@
 package com.dimthomas.borutoapp.presentation.screens.splash
 
 import android.content.res.Configuration
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.dimthomas.borutoapp.R
-import com.dimthomas.borutoapp.ui.theme.Purple40
 import com.dimthomas.borutoapp.ui.theme.Purple500
 import com.dimthomas.borutoapp.ui.theme.Purple700
-import com.dimthomas.borutoapp.ui.theme.Purple80
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    Splash()
+
+    val degrees = remember { Animatable(0f) }
+
+    LaunchedEffect(key1 = true) {
+        degrees.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+        )
+    }
+
+    Splash(degrees = degrees.value)
 }
 
 @Composable
-fun Splash() {
+fun Splash(degrees: Float) {
     if (isSystemInDarkTheme()) {
-        Box(modifier = Modifier
-            .background(Color.Black)
-            .fillMaxSize(),
+        Box(
+            modifier = Modifier
+                .background(Color.Black)
+                .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
             Image(
+                modifier = Modifier.rotate(degrees = degrees),
                 painter = painterResource(id = R.drawable.ic_logo),
-                contentDescription = null)
+                contentDescription = null
+            )
         }
     } else {
-        Box(modifier = Modifier
-            .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
-            .fillMaxSize(),
+        Box(
+            modifier = Modifier
+                .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
+                .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
             Image(
+                modifier = Modifier.rotate(degrees = degrees),
                 painter = painterResource(id = R.drawable.ic_logo),
-                contentDescription = null)
+                contentDescription = null
+            )
         }
     }
 }
@@ -54,11 +75,11 @@ fun Splash() {
 @Preview
 @Composable
 private fun SplashScreenPreview() {
-    Splash()
+    Splash(degrees = 0f)
 }
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 private fun SplashScreenDarkPreview() {
-    Splash()
+    Splash(degrees = 0f)
 }
